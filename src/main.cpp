1a499a4cedbc7ce52a53e9391af8c0f8ff42c299
rafaelmartinezchacon <rafael.martinez.chacon@estudiantat.upc.edu>
2021-06-06 17:30:40 +0200

    Added my project

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+volatile int interruptCounter;
+int totalInterruptCounter;
+hw_timer_t * timer = NULL;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+void IRAM_ATTR onTimer() {
+portENTER_CRITICAL_ISR(&timerMux);
+interruptCounter++;
+portEXIT_CRITICAL_ISR(&timerMux);
 }
-
+void setup() {
+Serial.begin(9600);
+timer = timerBegin(0, 80, true);
+timerAttachInterrupt(timer, &onTimer, true);
+timerAlarmWrite(timer, 1000000, true);
+timerAlarmEnable(timer);
+}
 void loop() {
-  // put your main code here, to run repeatedly:
+if (interruptCounter > 0) {
+portENTER_CRITICAL(&timerMux);
+interruptCounter--;
+portEXIT_CRITICAL(&timerMux);
+totalInterruptCounter++;
+Serial.print("An interrupt as occurred. Total number: ");
+Serial.println(totalInterruptCounter);
+}
 }
